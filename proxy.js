b2385db324e7dd432fa53c2647f737d054404581
nbriz <nickbriz@protonmail.com>
2023-11-02 19:43:04 -0500

    first attempt to osc back to server

--- proxy.js
+++ proxy.js
@@ -5,15 +5,27 @@ const OSC = require('node-osc')
 const MAX_IP = process.env.MAX_IP
 const MAX_PORT = process.env.MAX_PORT
 
+const oscServer = new OSC.Server(3333, '0.0.0.0', () => {
+  console.log('OSC Server is listening')
+})
+
+oscServer.on('message', function (msg) {
+  console.log(`Message: ${msg}`)
+  socket.emit('status', msg)
+  // oscServer.close()
+})
+
 const serverURL = `http://${process.env.SERVER_IP}:${process.env.SERVER_PORT}`
 console.log(`reaching out to ${serverURL}`)
 
 const socket = io(serverURL)
 socket.on('connect', () => {
   console.log('Connected to server ' + serverURL)
+
   socket.on('proxy-status', (data) => {
     console.log('got message', data)
     const client = new OSC.Client(MAX_IP, MAX_PORT)
     client.send(`/status ${data}`, 200, () => client.close())
   })
+
 })
--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = process.env.SERVER_PORT
-// const OSC = require('node-osc')
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`)
-})
+app.use(express.static('www'))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
